54889cae92aa5693fe4641180897815ac7ed3a28
deepakdroi <deepak.mahto706@gmail.com>
2019-04-04 16:50:15 +0530

    post validation and flashing error message if any at the time of submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,35 @@
 class PostsController < ApplicationController
+
 	def index
 		@posts = Post.all.order('created_at DESC') # gets all posts in descending order
 	end
 
 # used to create new post and its view contains form for the new post
-	def new	
-	end
+  def new
+    @post = Post.new 	
+  end
 
 # gets the params and saves it to the post table redirects it to the post where it shows the post which is posted
-	def create
-		@post =Post.new(post_params)
-		@post.save
-
-		redirect_to @post
-    end 
+  def create
+  	@post =Post.new(post_params)
+  	if @post.save
+  	  redirect_to @post
+  	else
+  		render 'new'
+  	end
+  end 
 
 # shows the post using the post_id
-    def show
-    	@post = Post.find(params[:id])
-    end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-    private
+  private
 
 # permiting the values to be asigned to the mentioned columns
-    def post_params
-    	params.require(:post).permit(:title, :content)
-    end 
+  def post_params
+   params.require(:post).permit(:title, :content)
+  end 
 end
 
 
