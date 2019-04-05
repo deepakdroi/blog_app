e00b64c5989913f1e62d391dc07bebcac2266270
deepakdroi <deepak.mahto706@gmail.com>
2019-04-05 08:05:34 +0530

    added crud operationd on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,34 @@ class PostsController < ApplicationController
 # gets the params and saves it to the post table redirects it to the post where it shows the post which is posted
   def create
   	@post =Post.new(post_params)
+
   	if @post.save
   	  redirect_to @post
   	else
   		render 'new'
   	end
   end 
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post = update[params [:post].permit(:title, :content)]
+      redirect_to @post
+    else
+    	render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+
+  	redirect_to root_path
+  end
 
 # shows the post using the post_id
   def show
